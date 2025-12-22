c9256b00cd867b280af4c7acc684af418a3f8f8a
Trang Trieu <trang.trieu@hotmail.com>
2025-12-22 07:44:50 -0500

    Time: 53 ms (69.93%) | Memory: 54.2 MB (85.93%) - LeetSync

---
+++ 2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    // return a new promise which is sum of promise 1 and promise 2
+    let value1 = await promise1;
+    let value2 = await promise2;
+    
+    return value1 + value2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
